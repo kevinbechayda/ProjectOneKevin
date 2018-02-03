dfddfe17e81ca9a47fdc632ce1bc68449579ce95
Kevin Bechayda <kevinbechayda@gmail.com>
2018-02-03 11:56:48 -1000

    First run through project 1

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -12,6 +12,38 @@ var thirdString = "Aloha"
 
 //Variable Coding Challenge #2
 
-let fahrenheit = 50
-let celsius = (1.8*fahrenheit) + 32
-prinntIn(celsius)
+let fahrenheit = 50.0
+let celsius = (fahrenheit - 32) * (5/9)
+let message = "Today is \(celsius) degrees celsius"
+print(message)
+
+//Boolean Variables Coding Challenge #1
+
+var numberOne = 3
+var numberTwo = 3
+var numberThree = numberOne == numberTwo
+
+//If statement Coding Challenge #1
+
+var strOne = "Wassup"
+var strTwo = "Sup bruh"
+
+var lengthOne = strOne.characters.count
+var lengthTwo = strTwo.characters.count
+
+
+var strOneLonger = lengthOne > lengthTwo
+if strOneLonger {
+    print("String one has more characters")
+}
+else {
+    print("String one doesn't have more characters")
+}
+
+var strTwoLonger = lengthTwo > lengthOne
+if strTwoLonger {
+    print("String two has more characters")
+}
+else {
+    print("String two doesn't have more characters")
+}
