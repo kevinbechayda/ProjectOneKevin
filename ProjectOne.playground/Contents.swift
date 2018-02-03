a26b0c816ba7268e385628c5a597ad183f9cb227
Kevin Bechayda <kevinbechayda@gmail.com>
2018-02-02 15:50:51 -1000

    Stuck on Part 5 #3

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -3,3 +3,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Variable Coding Challenge #1
+
+var firstString = "Hi"
+let secondString = "Hello"
+var thirdString = "Aloha"
+
+//Variable Coding Challenge #2
+
+let fahrenheit = 50
+let celsius = (1.8*fahrenheit) + 32
+prinntIn(celsius)
